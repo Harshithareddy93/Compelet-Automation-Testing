b6b8405dd5337ec202cdeae9b68c714cbca52635
Harshithareddy93 <harshithareddy9859@gmail.com>
2026-05-07 23:21:51 +0530

    changing into grid

--- conftest.py
+++ conftest.py
@@ -1,12 +1,11 @@
 import pytest
-
 from utils.driver_factory import get_driver
 
 
 @pytest.fixture(scope="function")
 def driver():
 
-    driver = get_driver(env="grid")
+    driver = get_driver(env="local")
 
     yield driver
 
--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,56 +1,24 @@
-import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-# Hide webdriver-manager logs
-os.environ["WDM_LOG"] = "0"
 
 
-BASE_URL = "https://practice.expandtesting.com/notes/app"
+def get_driver(env="local"):
 
-
-def get_driver(env="grid"):
-
-    chrome_options = Options()
-
-    chrome_options.add_argument("--start-maximized")
-    chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-
-    # Hide browser logs
-    chrome_options.add_experimental_option(
-        "excludeSwitches",
-        ["enable-logging"]
-    )
-
-    chrome_options.add_experimental_option(
-        "useAutomationExtension",
-        False
-    )
+    options = Options()
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-notifications")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
 
     if env == "grid":
 
         driver = webdriver.Remote(
             command_executor="http://localhost:4444/wd/hub",
-            options=chrome_options
+            options=options
         )
 
     else:
 
-        service = Service(
-            ChromeDriverManager().install()
-        )
-
-        service.creationflags = 0x08000000
-
-        driver = webdriver.Chrome(
-            service=service,
-            options=chrome_options
-        )
-
-
-    driver.get(BASE_URL)
+        driver = webdriver.Chrome(options=options)
 
     return driver
