c8b5fe3b43508086fc8ecececd397b63ce2ec170
Harshithareddy93 <harshithareddy9859@gmail.com>
2026-05-08 00:46:43 +0530

    Switched from Selenium Grid to local ChromeDriver

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,31 +1,16 @@
-import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
-@pytest.fixture(scope="function")
-def driver():
+def get_driver():
 
     options = Options()
 
-    # Headless mode for Jenkins
     options.add_argument("--headless=new")
-
-    # Required for Docker/Jenkins
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    # Optional stability settings
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
-
-    # Create Chrome driver
     driver = webdriver.Chrome(options=options)
 
-    # Maximize window
     driver.maximize_window()
 
-    yield driver
-
-    # Close browser after test
-    driver.quit()
+    return driver
