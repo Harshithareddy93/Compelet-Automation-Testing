5cc37ceb07af34b96394715d32a6420b6b8d5c9e
Harshithareddy93 <harshithareddy9859@gmail.com>
2026-05-07 23:38:50 +0530

    Fixed Selenium remote driver for Jenkins Docker

--- conftest.py
+++ conftest.py
@@ -1,11 +1,10 @@
 import pytest
 from utils.driver_factory import get_driver
 
-
 @pytest.fixture(scope="function")
 def driver():
 
-    driver = get_driver(env="local")
+    driver = get_driver(env="docker")
 
     yield driver
 
--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,24 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
 def get_driver(env="local"):
 
     options = Options()
-    options.add_argument("--start-maximized")
-    options.add_argument("--disable-notifications")
-    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless=new")
 
-    if env == "grid":
-
+    if env == "docker":
         driver = webdriver.Remote(
             command_executor="http://localhost:4444/wd/hub",
             options=options
         )
 
     else:
-
         driver = webdriver.Chrome(options=options)
 
+    driver.maximize_window()
     return driver
