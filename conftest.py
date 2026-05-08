6e1a55daec9d56db92d1f6bbc46ce7b2dd42d5fb
Harshithareddy93 <harshithareddy9859@gmail.com>
2026-05-08 12:40:05 +0530

    Update Chrome WebDriver initialization with Service

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="function")
@@ -13,7 +15,10 @@ def driver():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
 
     yield driver
 
