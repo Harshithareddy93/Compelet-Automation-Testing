2f7fa2157c0e0259971e0f23d9705a0b30c2aaaa
Harshithareddy93 <harshithareddy9859@gmail.com>
2026-05-08 11:21:58 +0530

    MCP

--- conftest.py
+++ conftest.py
@@ -8,15 +8,12 @@ def driver():
 
     options = Options()
 
-    options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--start-maximized")
+   
 
     driver = webdriver.Chrome(options=options)
 
-    driver.maximize_window()
+   
 
     yield driver
 
